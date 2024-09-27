021aef59ade8f1ec025b89a89adb5aed6f6f9f27
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-09-27 15:30:03 +1000

    get the currentQ

--- src/assets/components/Question.jsx
+++ src/assets/components/Question.jsx
@@ -4,28 +4,28 @@ import DataQuestions from "../../utils/dataQuestion";
 console.log(DataQuestions[0].question);
 
 function Question() {
-  const [question, setQuestion] = useState(0);
-  const [resultQ, setResultQ] = useState(0);
   const [currentQ, setCurrentQ] = useState(0);
+  const [resultQ, setResultQ] = useState(0);
+  const [questionIndex, setQuestionIndex] = useState(0);
 
   return (
     <div>
       <section className="section-question">
-        <h2>{DataQuestions[0].question}</h2>
+        <h2>{DataQuestions[currentQ].question}</h2>
       </section>
       <section className="section-answers">
         {/* Criar um map() */}
         <p>
           <input type="radio" name="answer" id="answer-one" />
-          {DataQuestions[0].answers[0]}
+          {DataQuestions[currentQ].answers[0]}
         </p>
         <p>
           <input type="radio" name="answer" id="answer-two" />
-          {DataQuestions[0].answers[1]}
+          {DataQuestions[currentQ].answers[1]}
         </p>
         <p>
           <input type="radio" name="answer" id="answer-three" />
-          {DataQuestions[0].answers[2]}
+          {DataQuestions[currentQ].answers[2]}
         </p>
       </section>
     </div>
