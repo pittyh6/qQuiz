519579f2c7f5143f61734c5d396caa449d644654
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-10-11 14:51:03 +1100

    clean

--- src/assets/components/Question.jsx
+++ src/assets/components/Question.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import DataQuestions from "../../utils/dataQuestion";
 import Result from "../components/Result";
 
-console.log("DataQuestions.length: ", DataQuestions.length);
-
 function Question() {
   const [currentQ, setCurrentQ] = useState(0);
   const [resultQ, setResultQ] = useState(0);
@@ -12,20 +10,17 @@ function Question() {
   function nextQuestion() {
     if (currentQ < DataQuestions.length - 1) {
       setCurrentQ(currentQ + 1);
-      console.log("currentQ: ", currentQ);
     } else {
       console.log("Questions Done!");
       //get the block/section .question-box and remove all elements and add the result.
       const questionBox = document.getElementById("qt-box");
       console.log(questionBox.innerHTML);
       questionBox.innerHTML = "Your total is: " + resultQ;
+      <Result />;
     }
   }
   function getAnswer(e) {
     setQuestionResult(e.target.value);
-    // const answer = e.target.value;
-    // console.log("answer: ", answer);
-    // return answer;
   }
   function handleAnswer() {
     console.log(questionResult);
@@ -44,7 +39,6 @@ function Question() {
         <h2>{DataQuestions[currentQ].question}</h2>
       </section>
       <section className="section-answers">
-        {/* Criar um map() */}
         {DataQuestions[currentQ].answers.map(function (answer, index) {
           return (
             <p key={index}>
