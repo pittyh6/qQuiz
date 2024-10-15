5f6389bdde84f121ec9fc992be7a39a094c3cb4f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-10-15 15:36:23 +1100

    css result

--- src/assets/components/Result.jsx
+++ src/assets/components/Result.jsx
@@ -4,13 +4,10 @@ export default function Result({ resultQ }) {
   return (
     <main>
       <section className="result-page">
-        <h2>Well Done</h2>
+        <h2>You did!!!!</h2>
         <p>Your total is: {resultQ}</p>
       </section>
-      <button
-        className="btn btn-restart"
-        onClick={() => window.location.reload()}
-      >
+      <button className="btn btn-next" onClick={() => window.location.reload()}>
         New Game
       </button>
     </main>
