3a699c9dc2664610e8bd3e7fc7c4b280b672e29d
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-10-15 15:29:04 +1100

    state to change page

--- src/assets/components/Question.jsx
+++ src/assets/components/Question.jsx
@@ -6,64 +6,64 @@ function Question() {
   const [currentQ, setCurrentQ] = useState(0);
   const [resultQ, setResultQ] = useState(0);
   const [questionResult, setQuestionResult] = useState("");
+  const [quizFinished, setQuizFinished] = useState(false);
 
   function nextQuestion() {
     if (currentQ < DataQuestions.length - 1) {
       setCurrentQ(currentQ + 1);
     } else {
       console.log("Questions Done!");
-      //get the block/section .question-box and remove all elements and add the result.
-      const questionBox = document.getElementById("qt-box");
-      console.log(questionBox.innerHTML);
-      questionBox.innerHTML = "Your total is: " + resultQ;
-      <Result />;
+      setQuizFinished(true);
     }
   }
   function getAnswer(e) {
     setQuestionResult(e.target.value);
   }
   function handleAnswer() {
-    console.log(questionResult);
-    if (questionResult == DataQuestions[currentQ].answerCorrect) {
-      console.log("Answer is correct: ", DataQuestions[currentQ].answerCorrect);
+    if (questionResult === DataQuestions[currentQ].answerCorrect) {
       setResultQ((resultQ) => resultQ + 1);
-      console.log("Answer totals: ", resultQ);
     } else {
       console.log("wrong: ", DataQuestions[currentQ].answerCorrect);
     }
   }
 
   return (
-    <div id="qt-box">
-      <section className="section-question">
-        <h2>{DataQuestions[currentQ].question}</h2>
-      </section>
-      <section className="section-answers">
-        {DataQuestions[currentQ].answers.map(function (answer, index) {
-          return (
-            <p key={index}>
-              <button
-                className="btn btn-answers"
-                onClick={getAnswer}
-                value={answer}
-              >
-                {answer}
-              </button>
-              <br />
-            </p>
-          );
-        })}
-      </section>
-      <button
-        className="btn btn-next"
-        onClick={function (event) {
-          handleAnswer();
-          nextQuestion();
-        }}
-      >
-        Next
-      </button>
-    </div>
+    <>
+      {quizFinished ? (
+        <Result resultQ={resultQ} />
+      ) : (
+        <div id="qt-box">
+          <section className="section-question">
+            <h2>{DataQuestions[currentQ].question}</h2>
+          </section>
+          <section className="section-answers">
+            {DataQuestions[currentQ].answers.map(function (answer, index) {
+              return (
+                <p key={index}>
+                  <button
+                    className="btn btn-answers"
+                    onClick={getAnswer}
+                    value={answer}
+                  >
+                    {answer}
+                  </button>
+                  <br />
+                </p>
+              );
+            })}
+          </section>
+          <button
+            className="btn btn-next"
+            onClick={function (event) {
+              handleAnswer();
+              nextQuestion();
+            }}
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/assets/components/Result.jsx
+++ src/assets/components/Result.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-export default function Result() {
-  console.log("entrou no Result.jsx");
-  const questionBox = document.getElementById("qt-box");
-  console.log(questionBox.innerHTML);
-  questionBox.innerHTML = "Your total is: " + resultQ;
+export default function Result({ resultQ }) {
   return (
     <main>
       <section className="result-page">
         <h2>Well Done</h2>
-        <p>Your total is:</p>
+        <p>Your total is: {resultQ}</p>
       </section>
-      <button className="btn btn-restart">New Game</button>
+      <button
+        className="btn btn-restart"
+        onClick={() => window.location.reload()}
+      >
+        New Game
+      </button>
     </main>
   );
 }
